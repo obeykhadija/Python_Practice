c11966a7ddf0c99b5c4efefe8e29bfda923f9e68
Khadija <81998785+obeykhadija@users.noreply.github.com>
2021-04-23 15:02:21 -0400

    Update mypython.py

--- mypython.py
+++ mypython.py
@@ -1,4 +1,22 @@
-#Display the Output
-x = 8
-y = 2
-print(x + y)
+# Python Practice
+
+# Organizing Data
+#Example: Highscore Comparison
+user_scores = [12, 42, 55, 100, 11, 22]
+highest = user_scores[0]
+
+for score in user_scores:
+  if score > highest:
+    highest = score
+ 
+print(f'Highest score: {highest}')
+
+#Example: Updating Humidity Data
+humidity_level = [87, 83, 89, 88, 87]
+humidity_level.insert(0, 90)
+humidity_level.pop()
+
+print(humidity_level)
+ 
+#Example: Latest Stock Trends
+
