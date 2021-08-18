f7aa9476c14270adfecae55b2d7181fa9ddf0e97
Khadija <81998785+obeykhadija@users.noreply.github.com>
2021-08-18 18:29:31 +0300

    Create classes_OOP.py

---
+++ classes_OOP.py
@@ -0,0 +1,132 @@
+#Classes and OOP
+#Vending Machine Code
+class Soda_Machine:
+  paid = False
+  balance = 0
+
+  def eject_soda(self):
+    if self.paid == False:
+      print('Please insert money')
+    else:
+      print('Enjoy the soda!')
+
+  def pay(self, amount):
+    self.balance += amount
+    
+  def select_soda(self):
+    if self.balance >= 1:
+      self.paid = True
+      self.balance -= 1
+    else:
+      self.paid = False
+
+soda = Soda_Machine()
+soda.pay(1)
+soda.select_soda()
+soda.eject_soda()
+
+#Archery Game
+class Archer:
+    def __init__(self):
+        self.arrows = 10
+        self.points = 0
+        self.empty = False
+
+    def shoot(self):
+        if self.arrows > 0:
+            print('Aim... and shoot!')
+            self.arrows -= 1
+        else:
+            self.empty = True
+    
+    def bullseye(self):
+        self.points +=3
+    
+    def reload(self):
+        if self.empty == True:
+            self.arrows = 10
+            self.empty = False
+
+archer = Archer()
+for arrow in range(1,archer.arrows):
+    archer.shoot()
+    archer.bullseye()
+    archer.reload()
+print(archer.points)
+
+#Role Playing Game (RPG)
+class Player:
+  def __init__(self, name):
+    self.name = name
+
+class Computer(Player):
+  def __init__(self):
+    super().__init__("NPC")
+
+  def respond(self, player):
+    print("Hello", player.name, "I am", self.name)
+
+class User(Player):
+  def __init__(self, name, level):
+    super().__init__(name)
+    self.level = level
+
+  def greet(self):
+    print("Hi! What is your name?")
+
+computer = Computer()
+user = User("User", 1)
+user.greet()
+computer.respond(user)
+
+#Automobile Game
+class Automobile:
+  def go(self):
+    print('Racing forward')
+
+  def stop(self):
+    print('Stopping')
+
+class Motorcycle(Automobile):
+  def wheelie(self):
+    print('Popping a wheelie')
+
+class Racecar(Automobile):
+  def reverse(self):
+    print('Reversing')
+
+  def goTo100(self):
+    print('Increasing to 100 mph')
+
+motorcycle = Motorcycle()
+racecar = Racecar()
+motorcycle.go()
+motorcycle.wheelie()
+racecar.goTo100()
+racecar.stop()
+
+#Food Delivery App
+class FoodDelivery: 
+  def __init__(self, number, items): 
+    self.number = number 
+    self.items = items 
+ 
+  def submit_order(self): 
+    print(f"Submitting order: {self.number}") 
+ 
+  def make_food(self, item): 
+    print(f"Made {item}") 
+ 
+  def package_item(self, item): 
+    print(f"Packaging {item}") 
+     
+  def complete_order(self): 
+    self.submit_order() 
+    for i in self.items: 
+      self.make_food(i) 
+      self.package_item(i) 
+    print("Delivering order", self.number)
+items = ["pizza", "shawarma", "sushi"]
+number = 3
+food_delivery = FoodDelivery(number, items)
+food_delivery.complete_order()
